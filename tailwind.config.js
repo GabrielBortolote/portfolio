52d639cacb459d2fa51a9158c49de87787c49ae0
BortoBoy <gbortolote@gmail.com>
2023-07-26 17:39:28 -0300

    :tada: First commit
    Add vite+ react structure with tailwindcss installed as a DEV dependency.

---
+++ tailwind.config.js
@@ -0,0 +1,31 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        // define theme colors here
+        color1: "#001C30",
+        color2: "#176B87",
+        color3: "#64CCC5",
+        color4: "#DAFFFB"
+      },
+      fontFamily: {
+        poppins: ["Poppins", "sans-serif"],
+      },
+    },
+    screens: {
+      xs: "480px",
+      ss: "620px",
+      sm: "768px",
+      md: "1060px",
+      lg: "1200px",
+      xl: "1700px",
+    },
+  },
+  plugins: [],
+}
+
